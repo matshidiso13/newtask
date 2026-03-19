8cfd5877f31bea27429b567572a8e7fb8b13b2ae
matshidiso13 <you@example.com>
2026-03-19 11:45:12 +0200

    quiz formative

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
-    let video = document.getElementById("myVideo");
 
-    function playVideo() {
-        video.play();
-    }
+function checkQuiz() {
+  let score = 0;
 
-    function pauseVideo() {
-        video.pause();
-    }
+  let q1 = document.querySelector('input[name="q1"]:checked');
+  let q2 = document.querySelector('input[name="q2"]:checked');
+  let q3 = document.querySelector('input[name="q3"]:checked');
 
-    function restartVideo() {
-        video.currentTime = 0;
-        video.play();
-    }
+  if (q1 && q1.value === "correct") score++;
+  if (q2 && q2.value === "correct") score++;
+  if (q3 && q3.value === "correct") score++;
+
+  document.getElementById("result").innerText = "Your score is: " + score + "/3";
+}
